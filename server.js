f3f6a95e02e457e732abb295c2822aa77ce8417c
Ramkumar6299 <36751271+Ramkumar6299@users.noreply.github.com>
2018-02-25 20:17:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,19 +8,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+app.get('/articleone', function (req, res) {
+  res.send(my articles  filebase,here you will be served at best );
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
-
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
-
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);

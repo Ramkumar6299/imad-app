1461c6ff0ade0339564d9e691d771d9aa83a2d5c
Ramkumar6299 <36751271+Ramkumar6299@users.noreply.github.com>
2018-02-27 19:21:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,14 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 
 element.innerHTML=('New text');
+
+var img=document.getElementById('madi');
+var marginleft=0;
+function moveRight (){
+    marginleft=1+marginleft;
+    img.style.marginleft=marginleft+'px';
+    
+}
+img.onclick =function(){
+    var interval = setInterval(moveRight,50);
+};

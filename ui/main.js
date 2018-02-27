3881227486e9f11c4e9883ef8228dd840b982601
Ramkumar6299 <36751271+Ramkumar6299@users.noreply.github.com>
2018-02-27 19:38:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,13 +4,12 @@ var element = document.getElementById("main-text");
 
 element.innerHTML=('New text');
 
-var img=document.getElementById('madi');
+var img=document.getElementById("madi");
 var marginleft=0;
 function moveRight (){
     marginleft = marginleft + 1;
     img.style.marginleft= marginleft + 'px';
-    
-}
+    }
 img.onclick =function(){
     var interval = setInterval(moveRight,50);
 };

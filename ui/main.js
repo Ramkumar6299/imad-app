718969a6acc5592f0c38a70b3fcc19618c032d37
Ramkumar6299 <36751271+Ramkumar6299@users.noreply.github.com>
2018-02-27 20:37:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,6 @@ var element = document.getElementById("main-text");
 element.innerHTML=('New text');
 
 var img=document.getElementById("madi");
-var marginleft=0;
 
 img.onclick =function(){
     img.style.marginleft='100px';
